11543c247cc1f1d30d2ce48270406998165dd4fc
Mike-Fernandez <00145518@uca.edu.sv>
2019-11-12 22:44:32 -0600

    Functional redirect to pages

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET login page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('login');
+});
+
+router.post('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+//  return res.redirect('/');
 });
 
 module.exports = router;
