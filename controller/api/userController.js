467e711af4bd5a4c86e48dbaaf1aedb1ab032e3b
Mike-Fernandez <00145518@uca.edu.sv>
2019-11-16 12:15:21 -0600

    Add: metodos de controller(crear,buscar por uno, buscar todos)

--- controller/api/userController.js
+++ controller/api/userController.js
@@ -1,13 +1,43 @@
 const User = require('../../models/Usuario');
+var debug = require('debug')('proyectoWeb:user_controller');
 
-module.exports.getId = (req, res) => {
-    User.find({ username: req.params.username }, function (err, docs) {
-        if (err)
-            res.status(500).json(err);
-        else
-            res.status(200).json(docs);
-    });
+module.exports.getone = (req,res,next) => {
+    debug("Search User", req.params);
+    User.findOne({
+        username: req.params.username
+    }, "-password -login_count")
+    .then((foundUser)=> {
+        if(foundUser)
+            return res.status(200).json(foundUser);
+        else 
+            return res.status(400).json(null);
+    })
+    .catch(err=>{
+        next(err);
+    })
 }
+
+module.exports.getAll = (req,res,next) => {
+
+    var perPage = Number(req.query.size) || 10,
+        page = req.query.page > 0 ? req.query.page : 0;
+    var sortProperty = req.query.sortby || "createdAt",
+        sort = req.query.sort || "desc";
+
+    debug("User List", {size: perPage, page, sortby:sortProperty,sort});
+
+    User.find({}, "-password -login_count")
+        .limit(perPage)
+        .skip(perPage*page)
+        .sort({ [sortProperty]: sort})
+        .then((users) => {
+            return res.status(200).json(users);
+        }).catch(err=>{
+            next(err);
+        })
+}
+
+
 
 module.exports.create = (req, res) => {
     let insertUser = new User({
