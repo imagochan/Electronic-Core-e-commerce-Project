166d9db70fb78a8099b162b5de2f9bff6dd0e7e4
Mike-Fernandez <00145518@uca.edu.sv>
2019-11-20 16:46:41 -0600

    Added route register user

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controller/api/userController');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Express' });
 });
+
+router.post('/', function(req,res,next) {
+  userController.register(req,res,next);
+})
 
 module.exports = router;
