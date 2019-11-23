fe05596b5c17d649c7f0cdad36c228ff1b917c41
julio <00039718@uca.edu.sv>
2019-11-22 19:11:14 -0600

    added: more data to sql

--- electronic_core_data.sql
+++ electronic_core_data.sql
@@ -1,5 +1,10 @@
 insert into admin values ('adminelectronic','semeolvido','Alfredo','Olivo','7777-4567','support@electronicore.com');
 
-insert into cliente values ('miguel','1234','Miguel','Fernandez','miguel@gmail.com','m','Avenida Amazonas Casa, #4-3','5345-3443-7645-1234');
-insert into cliente values ('neko','root','Saul','Orellana','saul@gmail.com','m','Avenida Los Platanares Casa, #19','6545-9873-0934-8555');
-insert into cliente values ('jhonny','','Saul','Orellana','saul@gmail.com','m','Avenida Los Platanares Casa, #19','6545-9873-0934-8555');
+insert into cliente values ('miguel','1234','Miguel','Fernandez','miguel@gmail.com','m','Avenida Amazonas ,Casa #4-3','5345-3443-7645-1234');
+insert into cliente values ('neko','root','Saul','Orellana','saul@gmail.com','m','Avenida Los Platanares ,Casa #19','6545-9873-0934-8555');
+insert into cliente values ('jhonny','mypuppy','Juan','Perez','perezderez@hotmail.com','m','Barrio Sinfonias ,Casa #4-8','6456-8778-6577-4555');
+insert into cliente values ('sins','eldespije','Ernesto','Viera','ernes.viera@gmail.com','m','Colonia Los Castaños ,Casa #27','4453-9653-0569-8675');
+insert into cliente values ('milky','kumatora','Alegria','Hernandez','alehernan13@outlook.com','f','Caserio Matasano Casa, #52','7645-4543-4056-3565');
+
+insert into producto values ('Televisor Samsung 32"','tv con OS, procesador 2ghz e internet',599.99,7,'https://images-na.ssl-images-amazon.com/images/I/91DK0S6RvhL._SX425_.jpg');
+insert into producto values ('Samsung Galaxy A30','cpu octa-core, larga bateria, pantalla amoled',249.99,4,'https://images.samsung.com/is/image/samsung/latin-galaxy-a30-a305-sm-a305gzbktpa-frontblue-156933426?$PD_GALLERY_L_JPG$')
